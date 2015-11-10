41150abd1697f80b0e5b9589085aeb4dba539acb
Valabius <valabius@gmail.com>
2015-11-10 21:17:38 +0200

    first commit

---
+++ src/com/aaaaa/CurrentThreadDemo.java
@@ -0,0 +1,19 @@
+package com.aaaaa;
+
+
+public class CurrentThreadDemo {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        System.out.println("Текущий поток: " + t);
+        t.setName("Главный поток");
+        System.out.println("После изменения имени: " + t);
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток прерван");
+        }
+    }
+}

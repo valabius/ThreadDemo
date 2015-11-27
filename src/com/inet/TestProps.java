3ee2e845dd6a8958cdaad12bec526ede1ad8b823
Valabius <valabius@gmail.com>
2015-11-27 13:29:23 +0200

    Add properties, package inet. Add TestProps, EchoServer, PostTest

---
+++ src/com/inet/TestProps.java
@@ -0,0 +1,17 @@
+package com.inet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class TestProps {
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        InputStream in = Files.newInputStream(Paths.get("config.properties"));
+        properties.load(in);
+        String url = properties.remove("url").toString();
+        System.out.println(properties);
+    }
+}

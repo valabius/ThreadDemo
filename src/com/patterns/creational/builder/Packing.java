8bcc9faaa2ce5655ac757ea3ab0b70df49c3d674
Valabius <valabius@gmail.com>
2015-12-03 18:57:22 +0200

    Add Builder

---
+++ src/com/patterns/creational/builder/Packing.java
@@ -0,0 +1,5 @@
+package com.patterns.creational.builder;
+
+public interface Packing {
+    public String pack();
+}
---
+++ src/com/patterns/creational/builder/Wrapper.java
@@ -0,0 +1,9 @@
+package com.patterns.creational.builder;
+
+public class Wrapper implements Packing {
+
+    @Override
+    public String pack() {
+        return "Wrapper";
+    }
+}
